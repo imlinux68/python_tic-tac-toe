ae138f4d4971a7082f1933c4fe7600d4877ead17
imlinux68 <104739714+imlinux68@users.noreply.github.com>
2023-05-21 18:38:31 +0300

    Add files via upload

---
+++ mysql/game_score.sql
@@ -0,0 +1,9 @@
+use game_score;
+
+CREATE TABLE IF NOT EXISTS game_score(
+    player1_name VARCHAR(255), 
+    player2_name VARCHAR(255), 
+    player1_score VARCHAR(255), 
+    player2_score VARCHAR(255), 
+    winer VARCHAR(255)
+);
